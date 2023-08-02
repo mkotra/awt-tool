93a9a7f9135f2283b710c6efb3d8d84da2f39754
mkotra <kotra.michal@gmail.com>
2023-08-02 21:45:13 +0200

    initial commit

--- src/main/java/org/example/AppFrame.java
+++ src/main/java/org/example/AppFrame.java
@@ -1,32 +1,30 @@
 package org.example;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.Timer;
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.awt.Robot;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-public class AppFrame extends JFrame {
+public class AppFrame extends Frame {
 
     private static final Logger LOGGER = Logger.getLogger(AppFrame.class.getName());
 
     boolean enabled = true;
-    JButton button1 = button1();
-    JButton button2 = button2();
-    JLabel label = label();
+    Button startButton = startButton();
+    Button stopButton = stopButton();
+    Button exitButton = exitButton();
+    Label label = label();
 
     public AppFrame() {
-        add(button1);
-        add(button2);
+        add(startButton);
+        add(stopButton);
+        add(exitButton);
         add(label);
 
-        setSize(170,  200);
+        setSize(170, 180);
         setTitle("awt-tool");
         setLayout(null);
         setVisible(true);
@@ -34,25 +32,32 @@ public class AppFrame extends JFrame {
         start();
     }
 
-    private JButton button1() {
-        JButton button = new JButton("Start");
+    private Button startButton() {
+        Button button = new Button("Start");
         button.setBounds(40, 30, 80, 30);
         button.addMouseListener(new StartHandler());
         return button;
     }
 
-    private JButton button2() {
-        JButton button = new JButton("Stop");
+    private Button stopButton() {
+        Button button = new Button("Stop");
         button.setBounds(40, 70, 80, 30);
         button.addMouseListener(new StopHandler());
         button.setEnabled(false);
         return button;
     }
 
-    private JLabel label() {
-        JLabel label = new JLabel("Started", JLabel.CENTER);
-        label.setBounds(40, 120, 80, 15);
-        return  label;
+    private Button exitButton() {
+        Button button = new Button("Exit");
+        button.setBounds(40, 110, 80, 30);
+        button.addMouseListener(new ExitHandler());
+        return button;
+    }
+
+    private Label label() {
+        Label label = new Label("Stopped", Label.CENTER);
+        label.setBounds(40, 160, 80, 15);
+        return label;
     }
 
     private class StartHandler extends MouseAdapter {
@@ -69,17 +74,24 @@ public class AppFrame extends JFrame {
         }
     }
 
+    private class ExitHandler extends MouseAdapter {
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+            exit();
+        }
+    }
+
     private void start() {
         label.setText("Started");
         LOGGER.info("Started!");
         enabled = true;
-        button1.setEnabled(false);
-        button2.setEnabled(true);
+        startButton.setEnabled(false);
+        stopButton.setEnabled(true);
         try {
             final Robot robot = new Robot();
             final Timer timer = new Timer((int) TimeUnit.SECONDS.toMillis(5), null);
             timer.addActionListener(l -> {
-                if(enabled) {
+                if (enabled) {
                     Point location = MouseInfo.getPointerInfo().getLocation().getLocation();
                     robot.mouseMove(location.x + 15, location.y + 15);
                     robot.mouseMove(location.x, location.y);
@@ -98,7 +110,13 @@ public class AppFrame extends JFrame {
         label.setText("Stopped");
         LOGGER.info("Stopped!");
         enabled = false;
-        button1.setEnabled(true);
-        button2.setEnabled(false);
+        startButton.setEnabled(true);
+        stopButton.setEnabled(false);
+    }
+
+    private void exit() {
+        stop();
+        LOGGER.info("Exit!");
+        System.exit(0);
     }
 }
